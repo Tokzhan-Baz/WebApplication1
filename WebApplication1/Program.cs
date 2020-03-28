43ee6d19f7bf92282fabb2b610fa2f9a470334cf
Tokzhan-Baz <b.tokzhan24@gmail.com>
2020-03-28 13:06:34 +0600

    Добавьте файлы проекта.

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,21 @@
+
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
